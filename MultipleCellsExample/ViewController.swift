d460f6e3870b5cb00b43d1d28f1df60c7b23fdf4
Dante Solorio <ddasogaa@gmail.com>
2017-12-21 13:45:20 -0600

    Build multi cell table

--- MultipleCellsExample/ViewController.swift
+++ MultipleCellsExample/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var dynamicTable: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    fileprivate let viewModel = ProfileViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
+        
+        dynamicTable.dataSource = viewModel
+        
+        dynamicTable.estimatedRowHeight = 100
+        dynamicTable.rowHeight = UITableViewAutomaticDimension
+        
+        dynamicTable.register(AboutCell.nib, forCellReuseIdentifier: AboutCell.identifier)
+        dynamicTable.register(NamePictureCell.nib, forCellReuseIdentifier: NamePictureCell.identifier)
+        dynamicTable.register(FriendCell.nib, forCellReuseIdentifier: FriendCell.identifier)
+        dynamicTable.register(AttributeCell.nib, forCellReuseIdentifier: AttributeCell.identifier)
+        dynamicTable.register(EmailCell.nib, forCellReuseIdentifier: EmailCell.identifier)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    private func setupView(){
+        view.addSubview(dynamicTable)
+        dynamicTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        dynamicTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        dynamicTable.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        dynamicTable.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+    }    
 }
 
